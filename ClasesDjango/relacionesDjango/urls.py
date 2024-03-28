0d81679c0275b2e3b09c960028a98c7bba2f08e0
Miguelit09 <miguel.p0908@gmail.com>
2024-03-28 16:27:43 -0500

    Listar y editar cursos

--- ClasesDjango/relacionesDjango/urls.py
+++ ClasesDjango/relacionesDjango/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('mostrar_registrar/', views.mostrar_registrar, name="mostrar_registrar"),
     path('registrar_curso_url/<str:cn>/<str:d>/<str:m>', views.registrar_curso_url, name="registrar_curso_url"),
     path('registrar_curso_form/', views.registrar_curso_form, name="registrar_curso_form"),
+    path('listar_cursos/', views.listar_cursos, name="listar_cursos"),
+    path('mostrar_editar/', views.mostrar_editar, name="mostrar_editar"),
+    path('editar_curso/', views.editar_curso, name="editar_curso"),
 ]
--- ClasesDjango/relacionesDjango/views.py
+++ ClasesDjango/relacionesDjango/views.py
@@ -17,3 +17,27 @@ def registrar_curso_form(request):
     materia = request.POST['materia']
     Curso.objects.create(curso_nombre=curso_nombre, descripcion=descripcion, materia=materia)
     return HttpResponse("Se registró el curso por formulario")
+
+def listar_cursos(request):
+    obtener_cursos = Curso.objects.all()
+    return render(request, 'listar_cursos.html', {
+        'lista_cursos': obtener_cursos
+    })
+
+def mostrar_editar(request):
+    return render(request, 'editar_curso.html', {})
+
+def editar_curso(request):
+    id_curso = request.POST['id_curso']
+    curso_nombre = request.POST['curso_nombre']
+    descripcion = request.POST['descripcion']
+    materia = request.POST['materia']
+
+    editar = Curso.objects.get(id=id_curso)
+    editar.curso_nombre = curso_nombre
+    editar.descripcion = descripcion
+    editar.materia = materia
+
+    editar.save()
+
+    return HttpResponse("Se editó el curso por formulario")
